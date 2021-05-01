6bd526c7b07bdc232ff3eba50925b597d2d11789
Júlio Sartori <jsartori2007@gmail.com>
2021-05-01 13:18:12 -0300

    Auto mode

--- app.js
+++ app.js
@@ -46,60 +46,98 @@ function sendAnswer(id, result) {
             case 200:
                 console.log(`Success`);
                 break;
-        
+
             case 400:
                 console.error('Request Failed.\nIncorrect value in result; no ID specified; value is invalid');
-				break;
-            
+                break;
+
             case 404:
                 console.error('Request Failed.\nValue not found for specified ID');
                 break;
-            
+
             case 503:
                 console.error('Request Failed.\nError communicating with database');
                 break;
-                
+
             default:
                 console.error('Somehting else happened!');
                 break;
-        } 
+        }
 
         res.on('data', d => {
             process.stdout.write(d);
+            if (autoMode)
+                getTaskSubmitAnswer();
+            else
+                readAnotherLine();
         })
     });
 
     req.on('error', error => {
         console.error(error);
+        if (autoMode)
+            getTaskSubmitAnswer();
+        else
+            readAnotherLine(); 
     });
 
     req.write(data);
     req.end();
 }
 
-//main commands start here...
-const req = https.request({
-    hostname: 'interview.adpeai.com',
-    port: 443,
-    path: '/api/v1/get-task',
-    method: 'GET'
-}, res => {
+function getTaskSubmitAnswer() {
+    const req = https.request({
+        hostname: 'interview.adpeai.com',
+        port: 443,
+        path: '/api/v1/get-task',
+        method: 'GET'
+    }, res => {
+    
+        console.log(`statusCode: ${res.statusCode}`)
+    
+        let rawData = '';
+        res.on('data', (chunk) => (rawData += chunk));
+        res.on('end', () => {
+            let data = JSON.parse(rawData);
+            let result = executeCalculation(data);
+            console.log('result', result);
+    
+            sendAnswer(data.id, result);
+        });
+    })
+    
+    req.on('error', error => {
+        console.error(error);
+        readAnotherLine(); 
+    })
+    
+    req.end();        
+}
 
-    console.log(`statusCode: ${res.statusCode}`)
+var autoMode = false;
 
-    let rawData = '';
-    res.on('data', (chunk) => (rawData += chunk));
-    res.on('end', () => {
-        let data = JSON.parse(rawData);
-        let result = executeCalculation(data);
-        console.log('result', result);
-
-        sendAnswer(data.id, result);
-    });
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
 
-req.on('error', error => {
-    console.error(error)
-})
-
-req.end();
+var readAnotherLine = function () {
+    readline.question(`Get another task and send answer? (Y, N or AUTO for automatic mode): `, command => {
+        if(command === 'Y' || command === 'y') {
+            getTaskSubmitAnswer();
+        }
+        else if(command === 'N' || command === 'n') {
+            console.log(`Closing application...`);
+            readline.close();
+        } else if(command === 'AUTO') {
+            console.log(`AUTO mode ON...`);
+            autoMode = true;
+            getTaskSubmitAnswer();
+        } else {
+            console.log('Wrong command. Expected Y or N!');
+            readAnotherLine(); 
+        }
+    })
+  };
+  
+  readAnotherLine(); 
